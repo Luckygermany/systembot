a9a59743edc2fc503fd201b7516cf5e46f28ea99
Lucky <79769081+Luckygermany@users.noreply.github.com>
2024-01-02 17:17:40 +0100

    Create cont.py

---
+++ docs/cont.py
@@ -0,0 +1,47 @@
+# Configuration file for the Sphinx documentation builder.
+
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# Project information
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+from datetime import date
+
+sys.path.insert(0, os.path.abspath("../"))
+
+
+
+project = "red Whimsy V2"
+copyright = f"{date.today().year}, Lucky"
+author = "Lucky"
+
+
+# General configuration
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
+]
+
+simplify_optional_unions = True
+
+autodoc_member_order = "bysource"
+
+
+
+
+# Options for HTML output and furo customisation
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# https://pradyunsg.me/furo/customisation/
+
+html_theme = "furo"
